9fed9f213a418e8ed9808b1dadd5013056fa0f25
konagaya <cresta522@gmail.com>
2021-12-18 15:02:04 +0900

    ts 追加

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -16,6 +16,7 @@ mix.js('resources/js/app.js', 'public/js').vue()
         require('postcss-import'),
         require('tailwindcss'),
     ])
+    .ts('resources/**/*', 'public/js/app.js')
     .webpackConfig(require('./webpack.config'));
 
 if (mix.inProduction()) {
